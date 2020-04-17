788fbd0c253176a4c00da90fbbdb92d24c879510
Martin Spicuzza <spicuzza157@gmail.com>
2020-04-17 15:21:21 -0500

    adding basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,42 @@ class ApplicationController < ActionController::API
     def check_if_exists(model)
         render json: {error: 400 , msg: "Model #{model.class} not found"} if !model
     end
+    
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def encode_token(payload)
+        JWT.encode(payload , ApplicationController.secret)
+    end
+
+    def decoded_token(token)
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+            JWT.decode(token , ApplicationController.secret , true , algorithm: 'HS256')
+            rescue JWT.DecodeError
+                nil
+            end
+        end
+    end
+
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        render json: {msg: 'Please Log In'} , status: unauthorized unless logged_in?
+            
+    private
+    def self.secret
+        "f0r_a11_7h3_m0n37_7h27_3R_7_h@d"
+    end
 end
---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,10 @@
+class AuthController < ApplicationController
+    def login
+    end
+
+    def oauth_login
+    end
+
+    def logout
+    end
+end
