cdffb8e584929e5a9a92d9cff29e044cb1064c61
Martin Spicuzza <spicuzza157@gmail.com>
2020-05-27 12:51:34 -0500

    monkey patching the create method of the project_update class to create some generated information

--- app/models/project_update.rb
+++ app/models/project_update.rb
@@ -1,14 +1,29 @@
 class ProjectUpdate < ApplicationRecord
-    before_create :generate_unique_report_number, :generate_update_report_content
+    before_create :generate_unique_report_number
 
     validates :report_number , uniqueness: true
 
     belongs_to :updatable , :polymorphic => true
     belongs_to :user
     belongs_to :project
-
+    
     def self.generate_template
         ProjectUpdate.create!(user_id: User.first.id , project_id: Project.first.id , updatable: Project.first)
+    end
+
+    def self.create(params = {} , message)
+        new_update = ProjectUpdate.new(user_id: params[:user_id] , updatable_type: params[:updatable_type] , updatable_id: params[:updatable_id])
+        new_update.generate_update_report_content(message)
+        if new_update.valid?
+            new_update.save
+        else
+            return { error: { message: "Project Update Params are invalid" , log: new_update.errors } }
+        end
+    end
+
+    def test_create
+        temp_params = {user_id: User.first.id , updatable: Project.first}
+        ProjectUpdate.create(my_params , "Creating New: #{updatable.class}")
     end
 
     private
@@ -21,10 +36,7 @@ class ProjectUpdate < ApplicationRecord
         self.report_number = random_int
     end
 
-    def generate_update_report_content
-        message = "You have a new Update to: #{self.project.title}!"
+    def generate_update_report_content(message = "You have a new Update to: #{self.project.title}!")
         self.report_content = message
-        message
-        # self.update(report_content: message)
     end
 end
